0df4c44b2ed6206bbb2cd4e597db197dc802dd05
tentusatish2006-commits <tentusatish2006@gmail.com>
2026-09-13 07:59:09 +0530

    Add live simulation problem photos (landslide, road damage, flood)

--- js/sim-problem-photos.js
+++ js/sim-problem-photos.js
@@ -1,12 +1,2 @@
-/* Problem-matched photos loader */
+/* Loads problem-matched field photos for Live Simulation */
 window.SIM_PROBLEM_PHOTOS = window.SIM_PROBLEM_PHOTOS || {};
-(function () {
-  function load(src) {
-    var s = document.createElement('script');
-    s.src = src;
-    s.async = false;
-    document.head.appendChild(s);
-  }
-  load('js/sim-photo-landslide.js');
-  load('js/sim-photo-road.js');
-})();
