4c932774417cebd9b317984081cf0d45cfde5cb7
tentusatish2006-commits <tentusatish2006@gmail.com>
2026-09-20 17:57:31 +0530

    Use reliable hosted field photos (pothole/road damage)

--- js/sim-photo-pothole.js
+++ js/sim-photo-pothole.js
@@ -1,1 +1,2 @@
-PLACEHOLDER_WILL_REPLACE
+window.SIM_PHOTO_POTHOLE = 'https://images.unsplash.com/photo-1515162816999-a0c47dc192f7?w=640&q=80';
+window.SIM_PHOTO_POTHOLE_FALLBACK = 'assets/pothole.jpg';
